4b3ae882d4874486a837757bd5bda06e87e1cae4
Teo Klestrup Röijezon <teo@nullable.se>
2016-08-26 12:40:38 +0200

    serve.sh: Set nix env variables

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Wrapper for stuff that runs the script directly through bash...
+export PATH=~/.nix-profile/bin/:$PATH
+export NIX_PATH=~/.nix-defexpr/channels/nixpkgs/
 ./build-and-serve.sh
